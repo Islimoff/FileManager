fd3105472553ab46ecd3f6baac9345c01767f53c
Ihor Sapronov <islimoff@gmail.com>
2020-04-01 23:23:17 +0300

    first commit

--- app/src/main/java/com/job4j/filemanager/MainActivity.java
+++ app/src/main/java/com/job4j/filemanager/MainActivity.java
@@ -1,7 +1,10 @@
 package com.job4j.filemanager;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,60 +14,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recycler;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        File directory = Environment.getExternalStorageDirectory();
-        File[] files = directory.listFiles();
-        recycler = findViewById(R.id.items_list);
-        recycler.setLayoutManager(new LinearLayoutManager(this));
-    }
+    protected void onCreate(@Nullable Bundle saved) {
+        super.onCreate(saved);
+        setContentView(R.layout.host_frg);
+        // my_child_toolbar is defined in the layout file
+//       Toolbar childToolbar = findViewById(R.id.toolbar_support);
+//        setSupportActionBar(childToolbar);
 
-    class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileHolder> {
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
 
-        private List<File> fileList;
-
-        public FileAdapter(List<File> fileList) {
-            this.fileList = fileList;
-        }
-
-        @NonNull
-        @Override
-        public FileAdapter.FileHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            View view = inflater.inflate(R.layout.file_manager, parent, false);
-            return new FileAdapter.FileHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull FileAdapter.FileHolder holder, int position) {
-            final File file=fileList.get(position);
-            TextView textView=holder.view.findViewById(R.id.file_name);
-            textView.setText(file.getName());
-        }
-
-        @Override
-        public int getItemCount() {
-            return fileList.size();
-        }
-
-        public class FileHolder extends RecyclerView.ViewHolder {
-
-            public View view;
-
-            public FileHolder(@NonNull View view) {
-                super(view);
-                this.view = itemView;
-            }
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentById(R.id.content) == null) {
+            fragmentManager.beginTransaction()
+                    .add(R.id.content, new FileListFragment())
+                    .commit();
         }
     }
 }
